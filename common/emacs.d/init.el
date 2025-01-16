4bec92046ae3c9bd5ddbb88a42048ea4a5d5e55e
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2025-01-16 23:16:18 +0900

    :sparkles (common/emacs.d/init.el) add emacs settings file

---
+++ common/emacs.d/init.el
@@ -0,0 +1,8 @@
+;;; Slime for common lisp
+(load (expand-file-name "~/.roswell/helper.el"))  ; slime 起動スクリプト
+;; (setq slime-lisp-implementations
+;;       `((ros ("ros" "run"))                       ; ros run の起動設定
+;;         (sbcl ("/opt/local/bin/sbcl"))
+;;         (abcl ("/opt/local/bin/abcl"))
+;;         (clisp ("/opt/local/bin/clisp"))))
+
