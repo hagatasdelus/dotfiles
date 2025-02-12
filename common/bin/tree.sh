67c0096d1b9e8276cdc1bc361cf8e41a2a61b5af
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2025-02-13 01:03:35 +0900

    :sparkles: (common/bin/tree.sh) tree update

---
+++ common/bin/tree.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    TARGET_DIRECTORY="."
+else
+    TARGET_DIRECTORY=$1
+fi
+
+cd $TARGET_DIRECTORY
+echo ./ \($(pwd)/\)
+
+find . -type d -name '.svn' -prune -or -type d -exec echo {}\.\~ \; -or \( -type f -o -type l \) -print |
+    sort |
+    sed '1d;s/^\.//;s/\/\([^/]*\)$/|-- \1/;s/\/[^/|]*/|-- /g' |
+    sed 's/\.\~$/\//'
+
+exit 0
