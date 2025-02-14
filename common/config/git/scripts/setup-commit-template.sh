80508c8622bc96a69a4272f2141e2407124e9993
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2025-02-15 06:24:51 +0900

    :tada: (commit-template, prepare-commit-msg, setup-commit-template.sh) Up
    Update setup-commit-template - cz he ikou dekiruyouni juunannsei wo motaseta syuusei

--- common/config/git/scripts/setup-commit-template.sh
+++ common/config/git/scripts/setup-commit-template.sh
@@ -1,13 +1,51 @@
 #!/bin/sh
 
-git config --global commit.template ~/.config/git/commit_template
+check_git_hooks_dir() {
+    if [ ! -d ~/.config/git/hooks ]; then
+        echo "~/.config/git/hooks directory does not exist."
+        exit 1
+    fi
+}
 
-if [ -d ".git" ]; then
-    cp ~/.config/git/hooks/prepare-commit-msg .git/hooks/
-    chmod +x .git/hooks/prepare-commit-msg
-    echo "Git hooks have been set up for this repository."
-else
+git config --local commit.template ~/.config/git/commit-template
+
+if [ ! -d ".git" ]; then
     echo "Current directory is not a git repository."
+    exit 1
 fi
+
+read -p "Do you use Git Hooks in this repository? (y/N): " ANS </dev/tty
+case "${ANS}" in
+[yY]*)
+    check_git_hooks_dir
+
+    for HOOK in ~/.config/git/hooks/*; do
+        if [ -f ${HOOK} ]; then
+            cp ${HOOK} .git/hooks/
+            HOOK_NAME=$(basename ${HOOK})
+            chmod +x .git/hooks/${HOOK_NAME}
+            echo "Installed: ${HOOK_NAME}"
+        fi
+    done
+    ;;
+[nN]*)
+    check_git_hooks_dir
+
+    for HOOK in ~/.config/git/hooks/*; do
+        if [ -f ${HOOK} ]; then
+            HOOK_NAME=$(basename ${HOOK})
+            if [ -f ".git/hooks/${HOOK_NAME}" ]; then
+                rm ".git/hooks/${HOOK_NAME}"
+                echo "Removed: ${HOOK_NAME}"
+            fi
+        fi
+    done
+    ;;
+*)
+    echo "Invalid input."
+    ;;
+esac
+
+echo "Setup done."
 
 exit 0
