f117c14947e9162a977ec589c3ceaa58b89a902f
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2025-02-25 05:08:18 +0900

    :sparkles: (notification.sh) Update notification - escape

--- common/bin/notification.sh
+++ common/bin/notification.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 # -*- coding: utf-8 -*-
 
 set -o pipefail
@@ -6,30 +6,16 @@ set -o pipefail
 FAILED_TITLE='Failed'
 SUCCESS_TITLE='Success'
 
-OLD_COMMAND_LINE="$@"
 NEW_COMMAND_LINE=''
 NUMBER_OF_ARGUMENTS=$#
 ARGUMENT_INDEX=1
 
 if [ ${NUMBER_OF_ARGUMENTS} -eq 0 ]; then
-    osascript -e "display notification \"Please specify the command\" with title \"${FAILED_TITLE}\" sound name \"Sosumi\""
+    osascript -e 'display notification "Please specify the command" with title "${FAILED_TITLE}" sound name "Sosumi"'
     exit 1
 fi
 
-for EACH_ARGUMENT in ${OLD_COMMAND_LINE}
-do
-    if [[ "${EACH_ARGUMENT}" =~ [[:space:]\'\"] ]]; then
-        NEW_COMMAND_LINE+="'${EACH_ARGUMENT//\'/\'\"\'\"\'}'"
-    else
-        NEW_COMMAND_LINE+="${EACH_ARGUMENT}"
-    fi
-    
-    if [ ${ARGUMENT_INDEX} -lt ${NUMBER_OF_ARGUMENTS} ]; then
-        NEW_COMMAND_LINE+=" "
-    fi
-    ARGUMENT_INDEX=$((ARGUMENT_INDEX + 1))
-done
-
+NEW_COMMAND_LINE=$(printf "%q " "$@")
 echo "+ ${NEW_COMMAND_LINE}"
 
 eval "${NEW_COMMAND_LINE}"
