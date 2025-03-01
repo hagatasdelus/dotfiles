31afae6b717c2684d15b7d2558dcd271535299c1
HaGaTa_ <gsiyakav.c08nk@gmail.com>
2025-03-01 14:37:16 +0900

    :sparkles: (statusline.lua) add lualine settings

--- common/config/nvim/lua/plugins/statusline.lua
+++ common/config/nvim/lua/plugins/statusline.lua
@@ -1,8 +1,47 @@
 return {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {},
-        event = "VeryLazy",
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    event = { "BufNewFile", "BufRead" },
+    opts = function()
+        return {
+            options = {
+                theme = "gruvbox",
+                component_separators = {},
+                section_separators = {},
+            },
+            sections = {
+                lualine_a = { "filename" },
+                lualine_b = { "branch" },
+                lualine_c = {
+                    "'%='",
+                    {
+                        "diff",
+                        symbols = { added = " ", modified = " ", removed = " " },
+                        separator = "  |  ",
+                    },
+                    {
+                        "diagnostics",
+                        symbols = { error = " ", warn = " ", info = " ", hint = " " },
+                    },
+                },
+                lualine_x = { "encoding", "fileformat" },
+                lualine_y = { "filetype", "searchcount" },
+                lualine_z = {},
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+        }
+    end,
+    config = function(_, opts)
+        local lualine = require("lualine")
+        lualine.setup(opts)
+    end,
 }
