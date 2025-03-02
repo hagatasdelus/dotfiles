a3a19e61d20a373547bc59cc881a9b597da594f4
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-02 16:26:52 +0900

    :sparkles: (setup_git.sh) add configuration items

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -2,5 +2,8 @@
 
 git config --global user.name HaGaTa_
 git config --global user.email hagatasdelus@gmail.com
+git config --global push.default current
 git config --global pull.ff only
 git config --global init.defaultBranch main
+git config --global core.ignorecase false
+git config --global color.ui true
