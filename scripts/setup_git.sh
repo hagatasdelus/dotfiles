64a284dc32a260311867e6c5811e64b5b95e56ca
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-02 15:50:14 +0900

    :tada: (setup_git.sh) add git setup

---
+++ scripts/setup_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+
+git config --global user.name HaGaTa_
+git config --global user.email hagatasdelus@gmail.com
+git config --global pull.ff only
+git config --global init.defaultBranch main
