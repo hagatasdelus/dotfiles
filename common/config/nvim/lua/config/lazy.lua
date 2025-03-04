98990243c46e484750114400b90dbc9020e9e186
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-04 18:22:00 +0900

    :art:  formatted - remove unused

--- common/config/nvim/lua/config/lazy.lua
+++ common/config/nvim/lua/config/lazy.lua
@@ -1,43 +1,43 @@
 -- lazy.nvim の repo の設定
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    '--branch=stable',
-    lazyrepo,
-    lazypath,
-  })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--branch=stable",
+        lazyrepo,
+        lazypath,
+    })
 
-  -- エラーが発生したらエラ〜メッセージを表示して終了させる
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out, 'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    -- エラーが発生したらエラ〜メッセージを表示して終了させる
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 local plugins = {
-    { import = 'plugins' },
+    { import = "plugins" },
 }
 
 local opts = {
-    root = vim.fn.stdpath('data') .. "/lazy",
-    lockfile = vim.fn.stdpath('config') .. "/lazy-lock.json",
+    root = vim.fn.stdpath("data") .. "/lazy",
+    lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
     concurrency = 10,
     checker = { enabled = false },
-    log = { level = 'info' },
+    log = { level = "info" },
 }
 
 -- lazy.nvim のセットアップ
-require('lazy').setup(plugins, opts)
+require("lazy").setup(plugins, opts)
