5dd15bb9043a24cd043ccd5e0bdd9e8ae894a9dd
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-05 19:01:52 +0900

    :gem: (bashrc course_setup.sh) refactor

--- common/bin/course_setup.sh
+++ common/bin/course_setup.sh
@@ -1,22 +1,21 @@
-#/bin/bash -xe
+#!/bin/bash -e
 # -*- coding: utf-8 -*-
 
 COMMAND_LINE="$@"
 NUMBER_OF_ARGUMENTS=$#
 
 if [ ${NUMBER_OF_ARGUMENTS} -lt 1 ]; then
-    echo "Error: Specify one or more course names" >&2
+    echo "Command Failed: Specify one or more course names." >&2
     exit 1
 fi
 
-for COURSE in ${COMMAND_LINE}
-do
+for COURSE in ${COMMAND_LINE}; do
     mkdir -p "${COURSE}/授業資料" "${COURSE}/レポート" "${COURSE}/テスト" "${COURSE}/過去問"
 
-    for i in $(seq -f '%02g' 1 15)
-    do
+    for i in $(seq -f '%02g' 1 15); do
         mkdir -p "${COURSE}/授業資料/${i}"
     done
+    echo "Course Setup Done >> ${COURSE}"
 done
 
 exit 0
