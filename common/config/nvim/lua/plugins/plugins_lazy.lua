2220c6f5bb553fd0dd821f3cc7313fc60f1fc01d
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-10 18:45:59 +0900

    :art: (barbar.lua) Separate files for each plugin #8

--- common/config/nvim/lua/plugins/plugins_lazy.lua
+++ common/config/nvim/lua/plugins/plugins_lazy.lua
@@ -76,19 +76,4 @@ return {
             })
         end,
     },
-    {
-        "romgrk/barbar.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        init = function()
-            vim.g.barbar_auto_setup = false
-        end,
-        opts = {
-            animation = true,
-            tabpages = true,
-            clickable = true,
-        },
-        version = "^1.0.0",
-    },
 }
