38a53d4af12a7be9a7a882b2fb33c1d4bb56b5b4
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-14 06:01:41 +0900

    :sparkles: (barbar.lua) update barbar config

--- common/config/nvim/lua/plugins/barbar.lua
+++ common/config/nvim/lua/plugins/barbar.lua
@@ -4,13 +4,23 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     init = function()
-        vim.g.barbar_auto_setup = false
+        vim.g.barbar_auto_setup = false 
     end,
     config = function()
         require("barbar").setup({
-            animation = true,
+            animation = false,
+
+            -- icons = {
+            --     separator = {left = '▎', right = ''},
+            --     separator_at_end = true,
+            -- },
+
+            -- auto_hide = true,
             tabpages = true,
             clickable = true,
+
+            -- highlight_visible = true,
+            -- highlight_alternate = false,
         })
     end,
     version = "^1.0.0",
