135564e1d379a05ad7e896c6e49f56e4ac05c922
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-18 08:22:52 +0900

    :sparkles: (options.lua) organised description

--- common/config/nvim/lua/core/options.lua
+++ common/config/nvim/lua/core/options.lua
@@ -1,70 +1,66 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- vim.opt.modifiable = true
+
 -- lang
 vim.cmd("language en_US.UTF-8") 
 -- vim.cmd("language ja_JP.UTF-8")
 
 -- File
--- vim.opt.fileencoding = "utf-8"
-vim.opt.swapfile = false -- スワップファイルを作成しない
+vim.opt.swapfile = false
 vim.opt.helplang = "ja,en"
-vim.opt.hidden = true -- バッファを切り替えるときにファイルを保存しなくてもOKに
+vim.opt.hidden = true
 
--- カーソルと表示
-vim.opt.cursorline = false -- カーソルがある行を強調
--- vim.opt.cursorcolumn = true -- カーソルがある列を強調
+-- Cursol
+vim.opt.cursorline = true
+-- vim.opt.cursorcolumn = true
+vim.opt.whichwrap = "b,s,h,l,[,],<,>,~"
 
--- クリップボード共有
-vim.opt.clipboard:append({ "unnamedplus" }) -- レジスタとクリップボードを共有
+-- Clipboard
+vim.opt.clipboard = "unnamedplus,unnamed"
 
--- メニューとコマンド
-vim.opt.wildmenu = true -- コマンドラインで補完
-vim.opt.cmdheight = 1 -- コマンドラインの表示行数
+-- Menu and Command
+vim.opt.wildmenu = true
+vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.showcmd = true
 vim.opt.confirm = true
 
--- 検索・置換え
-vim.opt.hlsearch = true -- ハイライト検索を有効
-vim.opt.incsearch = true -- インクリメンタルサーチを有効
-vim.opt.matchtime = 1 -- 入力された文字列がマッチするまでにかかる時間
+-- Search and Replace
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.matchtime = 1
 
--- カラースキーム
+-- ColorScheme
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 
--- インデント
-vim.opt.shiftwidth = 4 -- シフト幅4
-vim.opt.tabstop = 4 -- タブ幅4
-vim.opt.expandtab = true -- タブ文字をスペースに置き換える
-vim.opt.autoindent = true -- 自動インデントを有効にする
-vim.opt.smartindent = true -- インデントをスマートに調整する
+-- Indent
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
--- 表示
+-- Display
 vim.opt.number = true
 vim.opt.wrap = true
-vim.opt.showtabline = 2 -- タブラインを表示
-vim.opt.visualbell = true -- ビープ音を表示する代わりに画面をフラッシュ
-vim.opt.showmatch = true -- 対応する括弧をハイライト表示
+vim.opt.showtabline = 2
+vim.opt.visualbell = true
+vim.opt.errorbells = false
+vim.opt.showmatch = true
 
--- インタフェース
-vim.opt.winblend = 0 -- ウィンドウの不透明度
-vim.opt.pumblend = 0 -- ポップアップメニューの不透明度
-vim.opt.signcolumn = "yes" -- サインカラムを表示
-vim.opt.splitright = true -- splitは右に表示
+-- UI
+-- vim.opt.winblend = 0
+vim.opt.pumblend = 10
+vim.opt.signcolumn = "yes"
+vim.opt.splitright = true
 
----- 行番号の色を変更
+-- line number color
 vim.cmd("highlight LineNr guifg=#8a70ac")
 
--- カーソルの形状
 -- vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 
--- カーソル移動
-vim.opt.whichwrap = "b,s,h,l,[,],<,>,~"
-
--- signcolumnの優先順位（エラー/警告/ヒントの表示順）
+-- signcolumn priority (Error/Warning/Hints）
 vim.diagnostic.config({ severity_sort = true })
-
--- エラーの解消
-vim.opt.modifiable = true
