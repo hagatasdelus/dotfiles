1b8d572459c0a9b033249cffc13915fdaef0395c
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-18 07:03:10 +0900

    :sparkles: (init.lua, base.lua, user.lua) add base.lua
    do not load unnecessary plugins. change the order of core loading - need to set options and keymaps before plugins
    configuration

--- common/config/nvim/lua/user/ui.lua
+++ common/config/nvim/lua/user/ui.lua
@@ -1,5 +1,4 @@
-vim.o.background = "dark"
 -- vim.cmd.colorscheme "catppuccin-frappe"
 -- vim.cmd.colorscheme "tokyonight-night"
 -- vim.cmd([[colorscheme gruvbox]])
-vim.cmd("colorscheme kanagawa")
+-- vim.cmd("colorscheme kanagawa")
