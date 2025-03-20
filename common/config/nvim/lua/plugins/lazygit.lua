f25ebea5bbd8387ea2317c27594c250787b07ea7
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-20 23:57:24 +0900

    :sparkles: (lazygit.lua) fix pleblem with esc and q not working

--- common/config/nvim/lua/plugins/lazygit.lua
+++ common/config/nvim/lua/plugins/lazygit.lua
@@ -1,14 +1,33 @@
 return {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
-    },
-    keys = {
-        { "<leader>lg", "<Cmd>LazyGit<CR>", desc = "LazyGit" },
-    },
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	keys = {
+		{ "<leader>lg", "<Cmd>LazyGit<CR>", desc = "LazyGit" },
+	},
+	config = function()
+		vim.g.lazygit_floating_window_scaling_factor = 0.97
+
+		vim.api.nvim_create_augroup("LazygitKeyMapping", {})
+		vim.api.nvim_create_autocmd("TermOpen", {
+			group = "LazygitKeyMapping",
+			pattern = "*",
+			callback = function()
+				local filetype = vim.bo.filetype
+				-- filetypeにはlazygitが渡る。空文字ではない
+				if filetype == "lazygit" then
+					-- このkeymapが肝。なんでこれで動くのかは謎
+					vim.api.nvim_buf_set_keymap(0, "t", "<ESC>", "<ESC>", { silent = true })
+					-- <C-\><C-n>がNeovimとしてのESC。<ESC>はLazygitが奪う
+					vim.api.nvim_buf_set_keymap(0, "t", "<C-v><ESC>", [[<C-\><C-n>]], { silent = true })
+				end
+			end,
+		})
+	end,
 }
