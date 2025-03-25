67b428e442969a76cd41f6dc1f2d39edc814906d
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-25 09:22:06 +0900

    :tada: (copilot.lua, copilotchat.lua) add copilot config

--- common/config/nvim/lua/plugins/copilot.lua
+++ common/config/nvim/lua/plugins/copilot.lua
@@ -8,4 +8,56 @@ return {
         cmd = { "Copilot" },
         event = { "InsertEnter", "VeryLazy" },
     },
+    {
+        "zbirenbaum/copilot.lua",
+        enabled = true,
+        cmd = { "Copilot" },
+        event = { "InsertEnter", "VeryLazy" },
+        dependencies = {
+            { "zbirenbaum/copilot-cmp" },
+        },
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    keymap = {
+                        accept = "<C-g><C-g>",
+                        dismiss = "<C-e>",
+                        accept_word = false,
+                        accept_line = false,
+                        next = "<C-n>",
+                        prev = "<C-m>",
+                    },
+                },
+                panel = {
+                    -- enabled = false,
+                    auto_refresh = true,
+                    layout = {
+                        position = "right",
+                    },
+                    keymap = {
+                        jump_prev = "_",
+                        jump_next = "-",
+                        accept = "<CR>",
+                        refresh = false,
+                        open = "<C-S-CR>",
+                    },
+                },
+                copilot_node_command = "node",
+                filetypes = {
+                    yaml = true,
+                    markdown = true,
+                    help = false,
+                    gitcommit = true,
+                    gitrebase = true,
+                    hgcommit = false,
+                    svn = false,
+                    cvs = false,
+                    ["."] = false,
+                    ["*"] = true,
+                },
+            })
+        end,
+    },
 }
