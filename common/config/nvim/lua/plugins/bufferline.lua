dc47b6cb68cb5a44885ebe5cfcde4d3a52708267
HaGaTa_ <hagatasdelus@gmail.com>
2025-03-31 01:27:48 +0900

    :sparkles: (bufferline.lua) update tab highlight color

--- common/config/nvim/lua/plugins/bufferline.lua
+++ common/config/nvim/lua/plugins/bufferline.lua
@@ -1,4 +1,50 @@
- return {
+local highlights = {}
+
+highlights.yellow = {
+	separator = {
+		fg = "#1E1E1E",
+		bg = "#111111",
+	},
+	separator_selected = {
+		fg = "#1E1E1E",
+	},
+	background = {
+		fg = "#9A9A9A",
+		bg = "#111111",
+	},
+	buffer_selected = {
+		fg = "#FFDC7A",
+		bold = true,
+        italic = true,
+	},
+	fill = {
+		bg = "#1E1E1E",
+	},
+}
+
+highlights.yellow_green = {
+	separator = {
+		fg = "#B8B8B8",
+		bg = "#D6D6D6",
+	},
+	separator_selected = {
+		fg = "#B8B8B8",
+	},
+	background = {
+		fg = "#404040",
+		bg = "#D6D6D6",
+	},
+	buffer_selected = {
+		fg = "#CDFF04",
+		bold = true,
+		italic = true,
+	},
+	fill = {
+		bg = "#B8B8B8",
+	},
+}
+
+return {
 	"akinsho/bufferline.nvim",
 	event = { "BufAdd", "TabEnter" },
 	config = function()
@@ -10,30 +56,11 @@
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				color_icons = true,
-                indicator = {
-                    style = "underline",
-                },
-			},
-			highlights = {
-				separator = {
-					fg = "#1e1e1e",
-					bg = "#111111",
-				},
-				separator_selected = {
-					fg = "#1e1e1e",
-                },
-				background = {
-					fg = "#9a9a9a",
-					bg = "#111111",
-				},
-				buffer_selected = {
-					fg = "#ffdc7a",
-					bold = true,
-				},
-				fill = {
-					bg = "#1e1e1e",
+				indicator = {
+					style = "none",
 				},
 			},
+			highlights = highlights.yellow_green,
 		})
 	end,
 }
