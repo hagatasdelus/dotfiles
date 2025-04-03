df494b2d04dea1f153092c535c35bb3573561ae2
HaGaTa_ <hagatasdelus@gmail.com>
2025-04-03 22:14:15 +0900

    :pencil2: (barbar.lua) fix event typo

--- common/config/nvim/lua/plugins/barbar.lua
+++ common/config/nvim/lua/plugins/barbar.lua
@@ -3,9 +3,9 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    event = { BufAdd, TabEnter },
+    event = { "BufAdd", "TabEnter" },
     init = function()
-        vim.g.barbar_auto_setup = false 
+        vim.g.barbar_auto_setup = false
     end,
     config = function()
         require("barbar").setup({
