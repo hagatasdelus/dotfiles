a697ca7d4f4cb5bcd7a6397e1189561f7ae133e1
HaGaTa_ <hagatasdelus@gmail.com>
2025-04-07 23:54:37 +0900

    :art:  format kaketa

--- common/config/nvim/lua/plugins/copilotchat.lua
+++ common/config/nvim/lua/plugins/copilotchat.lua
@@ -1,18 +1,18 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		-- "zbirenbaum/copilot.lua",
-	},
-	build = "make tiktoken",
-	keys = {
-		{ "cpc", "CopilotChat", mode = "ca" },
-	},
-	opts = {
-		debug = true,
-		chat_autocomplete = true,
-		-- model = "gpt-4o",
-		model = "claude-3.7-sonnet",
-	},
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        -- "zbirenbaum/copilot.lua",
+    },
+    build = "make tiktoken",
+    keys = {
+        { "cpc", "CopilotChat", mode = "ca" },
+    },
+    opts = {
+        debug = true,
+        chat_autocomplete = true,
+        -- model = "gpt-4o",
+        model = "claude-3.7-sonnet",
+    },
 }
--- common/config/nvim/lua/plugins/gitsigns.lua
+++ common/config/nvim/lua/plugins/gitsigns.lua
@@ -1,16 +1,16 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPost" },
-	cmds = { "Gitsigns" },
-	opts = function()
-		return {
-			signcolumn = true,
-			numhl = true,
-			attach_to_untracked = true,
-		}
-	end,
-	config = function(_, opts)
-		local gitsigns = require("gitsigns")
-		gitsigns.setup(opts)
-	end,
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost" },
+    cmds = { "Gitsigns" },
+    opts = function()
+        return {
+            signcolumn = true,
+            numhl = true,
+            attach_to_untracked = true,
+        }
+    end,
+    config = function(_, opts)
+        local gitsigns = require("gitsigns")
+        gitsigns.setup(opts)
+    end,
 }
