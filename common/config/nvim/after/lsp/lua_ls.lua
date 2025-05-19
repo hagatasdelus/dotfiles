90e749b69812d7d60adf802d27376e755ef4ad21
HaGaTa_ <hagatasdelus@gmail.com>
2025-05-19 22:33:05 +0900

    feat: update cmd

--- common/config/nvim/after/lsp/lua_ls.lua
+++ common/config/nvim/after/lsp/lua_ls.lua
@@ -1,7 +1,7 @@
 ---@type vim.lsp.Config
 return {
-    -- cmd = { "lua-language-server" },
-    -- filetypes = { "lua" },
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
     -- root_markers = { ".git", ".luarc.json", ".luarc.jsonc", ".luacheckrc", "stylua.toml", "selene.toml", "selene.yml" },
     settings = {
         Lua = {
