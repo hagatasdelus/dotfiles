cbfd35cad4026bd5dc4088d3789979412ee942c2
HaGaTa_ <hagatasdelus@gmail.com>
2025-05-20 17:58:00 +0900

    feat: add vtsls settings

---
+++ common/config/nvim/after/lsp/vtsls.lua
@@ -0,0 +1,55 @@
+---@type vim.lsp.Config
+return {
+    cmd = { "vtsls", "--stdio" },
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+    },
+    settings = {
+        typescript = {
+            preferences = { preferTypeOnlyAutoImports = true },
+            preferGoToSourceDefinition = true,
+            importModuleSpecifier = "relative",
+            inlayHints = {
+                parameterNames = {
+                    enabled = "literals",
+                    suppressWhenArgumentMatchesName = true,
+                },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+            },
+        },
+        javascript = {
+            preferGoToSourceDefinition = true,
+        },
+    },
+    on_attach = function(client, bufnr)
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            convert = function(item)
+                return { abbr = item.label:gsub("%b()", "") }
+            end,
+        })
+    end,
+    ---@param bufnr number
+    ---@param callback fun(root_dir?: string)
+    root_dir = function(bufnr, callback)
+        local path = vim.api.nvim_buf_get_name(bufnr)
+        if path == "" then
+            return
+        end
+        local ts_marker = { "package.json" }
+        local ts_root = vim.fs.find(ts_marker,
+            { path = vim.fs.dirname(vim.fs.normalize(path)), upward = true, type = "file" })
+        if #ts_root > 0 then
+            return callback(vim.fs.dirname(ts_root[1]))
+        end
+    end,
+
+}
