d06794ff549f452ca450058fdee7b34bca2d461a
HaGaTa_ <hagatasdelus@gmail.com>
2025-05-20 17:57:47 +0900

    feat: add denols settings

---
+++ common/config/nvim/after/lsp/denols.lua
@@ -0,0 +1,39 @@
+---@type vim.lsp.Config
+return {
+    cmd = { "deno", "lsp" },
+    workspace_required = true,
+    settings = {
+        deno = {
+            lint = true,
+            unstable = false,
+        },
+    },
+    on_attach = function(client, bufnr)
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            convert = function(item)
+                return { abbr = item.label:gsub("%b()", "") }
+            end,
+        })
+    end,
+    ---@param bufnr number
+    ---@param callback fun(root_dir?: string)
+    root_dir = function(bufnr, callback)
+        local path = vim.api.nvim_buf_get_name(bufnr)
+        if path == "" then
+            return
+        end
+        local deno_marker = { "deno.json", "deno.jsonc", "deps.ts" }
+        local deno_root = vim.fs.find(deno_marker,
+            { path = vim.fs.dirname(vim.fs.normalize(path)), upward = true, type = "file" })
+        if #deno_root > 0 then
+            return callback(vim.fs.dirname(deno_root[1]))
+        end
+    end,
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+    },
+}
