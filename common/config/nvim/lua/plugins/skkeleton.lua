ec70e5b87a45545ccbcbe47c79d216d956647ad0
HaGaTa_ <hagatasdelus@gmail.com>
2025-06-28 11:41:38 +0900

    :seedling: (skkeleton.lua) introduce skkeleton

---
+++ common/config/nvim/lua/plugins/skkeleton.lua
@@ -0,0 +1,29 @@
+local jisyo_dir = vim.fn.expand("~/.local/share/skk")
+
+return {
+    "vim-skk/skkeleton",
+    event = "InsertEnter",
+    dependencies = {
+        "vim-denops/denops.vim",
+    },
+    cond = not use_in_vscode(),
+    config = function()
+        vim.fn["skkeleton#config"]({
+            eggLikeNewline = true,
+            globalDictionaries = {
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.L"),
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.emoji.utf8"),
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.law"),
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.propernoun"),
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.geo"),
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.station"),
+                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.jinmei"),
+            },
+            userDictionary = vim.fn.expand(jisyo_dir .. "/skk-jisyo.utf8"),
+            -- completionRankFile = vim.fn.expand(jisyo_dir .. "/rank.json"),
+            immediatelyCancel = false,
+            immediatelyOkuriConvert = true,
+            registerConvertResult = true,
+        })
+    end,
+}
