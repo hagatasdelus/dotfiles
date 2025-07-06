f45081a7ca0f43d7a906cd8955547176471ad267
HaGaTa_ <hagatasdelus@gmail.com>
2025-07-07 02:41:03 +0900

    feat(core/utils.lua): misucchatta!

--- common/config/nvim/lua/core/utils.lua
+++ common/config/nvim/lua/core/utils.lua
@@ -2,13 +2,12 @@ local M = {}
 
 ---@param file_path string
 M.open_file_with_quicklook = function(file_path)
-    -- if vim.fn.executable("qlmanage") == 1 then
-    --     local command = { "silent", "!qlmanage", "-p", vim.fn.shellescape(file_path), "&" }
-    --     vim.cmd(table.concat(command, " "))
-    -- else
-    --     vim.notify("QuickLook is not available on this system.", vim.log.levels.ERROR)
-    -- end
-    vim.cmd(("silent !qlmanage -p %s &"):format(file_path))
+    if vim.fn.executable("qlmanage") == 1 then
+        local command = { "silent", "!qlmanage", "-p", vim.fn.shellescape(file_path), "&" }
+        vim.cmd(table.concat(command, " "))
+    else
+        vim.notify("QuickLook is not available on this system.", vim.log.levels.ERROR)
+    end
 end
 
 return M
