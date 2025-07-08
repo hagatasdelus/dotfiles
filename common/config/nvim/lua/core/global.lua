abf8f43f21c353d7ec4e18cd0747c460202828d8
HaGaTa_ <hagatasdelus@gmail.com>
2025-07-08 23:37:24 +0900

    feat: move debounce global to utils

--- common/config/nvim/lua/core/global.lua
+++ common/config/nvim/lua/core/global.lua
@@ -12,21 +12,6 @@ _G.to_bool = function(value)
     end
 end
 
-_G.debounce = function(func, wait)
-    local timer_id
-    return function(...)
-        if timer_id ~ nil then
-            vim.uv.timer_stop(timer_id)
-        end
-        local args = { ... }
-        timer_id = assert(vim.uv.new_timer())
-        vim.uv.timer_start(timer_id, wait, 0, function()
-            func(unpack(args))
-            timer_id = nil
-        end)
-    end
-end
-
 _G.is_on_vscode = function()
     return to_bool(vim.g.vscode)
 end
--- common/config/nvim/lua/core/utils.lua
+++ common/config/nvim/lua/core/utils.lua
@@ -10,4 +10,20 @@ M.open_file_with_quicklook = function(file_path)
     end
 end
 
+function M.debounce(func, wait)
+    local timer_id
+    return function(...)
+        if timer_id ~ nil then
+            vim.uv.timer_stop(timer_id)
+        end
+        local args = { ... }
+        timer_id = assert(vim.uv.new_timer())
+        vim.uv.timer_start(timer_id, wait, 0, function()
+            func(unpack(args))
+            timer_id = nil
+        end)
+    end
+end
+
+
 return M
