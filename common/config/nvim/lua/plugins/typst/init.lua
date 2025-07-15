1a9b7974e76114a9cbb1a5763b32e1260e8f05da
HaGaTa_ <hagatasdelus@gmail.com>
2025-07-15 23:08:11 +0900

    feat(typst/init.lua): add typst-preview

--- common/config/nvim/lua/plugins/typst/init.lua
+++ common/config/nvim/lua/plugins/typst/init.lua
@@ -2,10 +2,20 @@
 return {
     {
         "kaarmu/typst.vim",
+        enabled = false,
         ft = { "typst" },
         -- lazy = false,
         -- config = function()
         --     vim.g.typst_pdf_viewer = ""
         -- end,
     },
+    {
+        "chomosuke/typst-preview.nvim",
+        enabled = true,
+        ft = { "typst" },
+        config = function()
+            require("typst-preview").setup({})
+        end,
+    },
+
 }
