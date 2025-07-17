814434043d0789e909e8744f3d8d8a24d2299c03
HaGaTa_ <hagatasdelus@gmail.com>
2025-07-18 05:36:44 +0900

    fire(core/keymaps.lua): good bye - unnecessary comment-out

--- common/config/nvim/lua/core/keymaps.lua
+++ common/config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,5 @@
--- Key mappings setup
 local vim = vim
 
--- local map = vim.api.nvim_set_keymap
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
@@ -35,30 +33,10 @@ map({ "n", "v" }, "X", '"_X', extend_opts("Delete to end of line without yanking
 map("t", [[<ESC><ESC>]], [[<C-\><C-n>]], opts)
 map("t", [[<C-[><C-[>]], [[<C-\><C-n>]], opts)
 
--- barbar
--- Move to previous/next
--- map("n", "<leader>,", "<Cmd>BufferPrevious<CR>", opts)
--- map("n", "<leader>.", "<Cmd>BufferNext<CR>", opts)
--- -- Re-order to previous/next
--- map("n", "<leader>>", "<Cmd>BufferMovePrevious<CR>", opts)
--- map("n", "<leader><", "<Cmd>BufferMoveNext<CR>", opts)
--- -- Goto buffer in position...
--- map("n", "<leader>1", "<Cmd>BufferGoto 1<CR>", opts)
--- map("n", "<leader>2", "<Cmd>BufferGoto 2<CR>", opts)
--- map("n", "<leader>3", "<Cmd>BufferGoto 3<CR>", opts)
--- map("n", "<leader>4", "<Cmd>BufferGoto 4<CR>", opts)
--- map("n", "<leader>5", "<Cmd>BufferGoto 5<CR>", opts)
--- map("n", "<leader>6", "<Cmd>BufferGoto 6<CR>", opts)
--- map("n", "<leader>7", "<Cmd>BufferGoto 7<CR>", opts)
--- map("n", "<leader>8", "<Cmd>BufferGoto 8<CR>", opts)
--- map("n", "<leader>9", "<Cmd>BufferGoto 9<CR>", opts)
--- map("n", "<leader>0", "<Cmd>BufferLast<CR>", opts)
 -- -- close-buffers
 map("n", "<A-c>", "<Cmd>BufferClose<CR>", extend_opts("Close Current Buffer"))
 map("n", "<A-w>", "<Cmd>BufferWipeout<CR>", extend_opts("Wipeout Current Buffer"))
 map("n", "<A-a>", "<Cmd>BufferCloseAllButCurrent<CR>", extend_opts("Close All Buffers Except Current"))
-
--- bufferline
 
 -- Gitsigns
 map("n", "]g", "<Cmd>Gitsigns next_hunk<CR>", extend_opts("Next Git Hunk"))
