f8f7d87722e9e2e6f3537af463e4b1467d5a5eb7
HaGaTa_ <hagatasdelus@gmail.com>
2025-07-25 21:05:51 +0900

    feat: remove lsp.format autocmd & add bufwritepre etc

--- common/config/nvim/lua/core/autocmds.lua
+++ common/config/nvim/lua/core/autocmds.lua
@@ -2,16 +2,23 @@
 vim.cmd('cnoreabbrev tn tabnew')
 vim.cmd('cnoreabbrev vs vsplit')
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-    pattern = { "*" },
-    callback = function()
-        vim.lsp.buf.format() { sync = false, timeout_ms = 20000 }
-    end,
-})
-
 vim.api.nvim_create_autocmd("TermOpen", {
     pattern = "*",
     callback = function()
         vim.cmd.startinsert()
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight on yank",
+    callback = function()
+        vim.highlight.on_yank()
     end,
 })
