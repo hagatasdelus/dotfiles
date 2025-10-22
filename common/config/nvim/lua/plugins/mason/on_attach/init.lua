7628e31123d90ed62252c9e9c837974b3141db48
HaGaTa_ <hagatasdelus@gmail.com>
2025-10-22 23:32:41 +0900

    chore: add diagnostic on_attach

--- common/config/nvim/lua/plugins/mason/on_attach/init.lua
+++ common/config/nvim/lua/plugins/mason/on_attach/init.lua
@@ -1,5 +1,6 @@
 local on_attach = function(client, bufnr)
     require("plugins.mason.on_attach.keymaps").on_attach(client, bufnr)
+    -- require("plugins.mason.on_attach.diagnostic").on_attach(client, bufnr)
     require("plugins.mason.on_attach.format").on_attach(client, bufnr)
 end
 
