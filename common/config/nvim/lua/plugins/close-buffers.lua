0cb780a7ae48009c49200eb9c710b6ff6878df3b
HaGaTa_ <hagatasdelus@gmail.com>
2025-10-29 23:20:00 +0900

    chore: udpate close-buffers github full path

--- common/config/nvim/lua/plugins/close-buffers.lua
+++ common/config/nvim/lua/plugins/close-buffers.lua
@@ -1,10 +1,10 @@
 return {
-    "kazhala/close-buffers.nvim",
+    "https://github.com/kazhala/close-buffers.nvim",
     cond = not is_on_vscode(),
     event = { "BufAdd", "TabEnter" },
     keys = {
-        { "<A-c>", "<Cmd>BufferClose<CR>",              desc = "Close Current Buffer" },
-        { "<A-w>", "<Cmd>BufferWipeout<CR>",            desc = "Wipeout Current Buffer" },
+        { "<A-c>", "<Cmd>BufferClose<CR>", desc = "Close Current Buffer" },
+        { "<A-w>", "<Cmd>BufferWipeout<CR>", desc = "Wipeout Current Buffer" },
         { "<A-a>", "<Cmd>BufferCloseAllButCurrent<CR>", desc = "Close All Buffers Except Current" },
     },
 }
