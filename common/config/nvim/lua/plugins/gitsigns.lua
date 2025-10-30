affcf03b35452174f368a692f9c489815553f98d
HaGaTa_ <hagatasdelus@gmail.com>
2025-10-30 23:54:04 +0900

    chore: update gitsigns github full path

--- common/config/nvim/lua/plugins/gitsigns.lua
+++ common/config/nvim/lua/plugins/gitsigns.lua
@@ -1,18 +1,18 @@
 return {
-    "lewis6991/gitsigns.nvim",
+    "https://github.com/lewis6991/gitsigns.nvim",
     event = { "BufReadPost" },
     cmds = { "Gitsigns" },
     keys = {
-        { "]g",         "<Cmd>Gitsigns next_hunk<CR>",       desc = "Next Git Hunk" },
-        { "[g",         "<Cmd>Gitsigns prev_hunk<CR>",       desc = "Previous Git Hunk" },
-        { "<C-g><C-a>", "<Cmd>Gitsigns stage_hunk<CR>",      desc = "Stage Git Hunk" },
-        { "<C-g><C-d>", "<Cmd>Gitsigns diffthis ~<CR>",      desc = "Diff Current File" },
-        { "<C-g><C-p>", "<Cmd>Gitsigns diffthis ~<CR>",      desc = "Preview Git Hunk" },
-        { "<C-g><C-q>", "<Cmd>Gitsigns setqflist<CR>",       desc = "Set Quickfix List" },
+        { "]g", "<Cmd>Gitsigns next_hunk<CR>", desc = "Next Git Hunk" },
+        { "[g", "<Cmd>Gitsigns prev_hunk<CR>", desc = "Previous Git Hunk" },
+        { "<C-g><C-a>", "<Cmd>Gitsigns stage_hunk<CR>", desc = "Stage Git Hunk" },
+        { "<C-g><C-d>", "<Cmd>Gitsigns diffthis ~<CR>", desc = "Diff Current File" },
+        { "<C-g><C-p>", "<Cmd>Gitsigns diffthis ~<CR>", desc = "Preview Git Hunk" },
+        { "<C-g><C-q>", "<Cmd>Gitsigns setqflist<CR>", desc = "Set Quickfix List" },
         { "<C-g><C-r>", "<Cmd>Gitsigns undo_stage_hunk<CR>", desc = "Undo Stage Hunk" },
-        { "<C-g>a",     "<Cmd>Gitsigns stage_buffer<CR>",    desc = "Stage Buffer" },
-        { "<C-g><C-a>", ":'<,'>Gitsigns stage_hunk<CR>",     mode = "x",                desc = "Stage Selected Hunk" },
-        { "<C-g><C-v>", "<Cmd>Gitsigns blame_line<CR>",      mode = { "n", "x" },       desc = "Blame Line" },
+        { "<C-g>a", "<Cmd>Gitsigns stage_buffer<CR>", desc = "Stage Buffer" },
+        { "<C-g><C-a>", ":'<,'>Gitsigns stage_hunk<CR>", mode = "x", desc = "Stage Selected Hunk" },
+        { "<C-g><C-v>", "<Cmd>Gitsigns blame_line<CR>", mode = { "n", "x" }, desc = "Blame Line" },
     },
     opts = function()
         return {
