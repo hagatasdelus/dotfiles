a02b48723688ed402d42b8da85e080c4f7cb2b02
HaGaTa_ <hagatasdelus@gmail.com>
2025-12-01 20:54:53 +0900

    refactor(autocmds): introduce create_autocmd wrapper and set up core autocommands
    - Wrap `nvim_create_autocmd` in a helper function `create_autocmd` to
    enforce the use of a central `core_autocmds` autogroup.

--- common/config/nvim/lua/core/autocmds.lua
+++ common/config/nvim/lua/core/autocmds.lua
@@ -1,22 +1,37 @@
 -- Command abbreviations
-vim.cmd('cnoreabbrev tn tabnew')
-vim.cmd('cnoreabbrev vs vsplit')
+vim.cmd("cnoreabbrev tn tabnew")
+vim.cmd("cnoreabbrev vs vsplit")
 
-vim.api.nvim_create_autocmd("TermOpen", {
+local augroup = vim.api.nvim_create_augroup("core_autocmds", { clear = true })
+
+local function create_autocmd(event, opts)
+    local final_opts = vim.tbl_extend("keep", { group = augroup }, opts)
+    vim.api.nvim_create_autocmd(event, final_opts)
+end
+
+create_autocmd("TermOpen", {
     pattern = "*",
+    desc = "Enter insert mode when opening a terminal",
     callback = function()
         vim.cmd.startinsert()
     end,
 })
 
-vim.api.nvim_create_autocmd("BufReadPost", {
+create_autocmd("BufReadPost", {
     pattern = "*",
+    desc = "Restore cursor position",
     callback = function()
-        vim.api.nvim_exec('silent! normal! g`"zv', false)
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local lcount = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+        -- vim.api.nvim_exec('silent! normal! g`"zv', false)
     end,
 })
 
-vim.api.nvim_create_autocmd("TextYankPost", {
+create_autocmd("TextYankPost", {
+    desc = "Highlight on yank",
     callback = function()
         vim.highlight.on_yank()
     end,
