c488454ab5468c630817e586201a4b8fde35b43b
HaGaTa_ <hagatasdelus@gmail.com>
2025-12-08 18:19:05 +0900

    chore(nvim): formatted

--- common/config/nvim/lua/core/keymaps.lua
+++ common/config/nvim/lua/core/keymaps.lua
@@ -5,17 +5,17 @@ map("n", "<CR><CR>", "<C-w><C-w>")
 map("i", "jj", "<ESC>")
 map("", "ss", "^")
 map("", ";;", "$")
-map({ "n", "v" }, "x", '"_x')  -- Delete without yanking
+map({ "n", "v" }, "x", '"_x') -- Delete without yanking
 map({ "n", "v" }, "X", '"_d$') -- Delete to end of line without yanking
-map("n", "Y", "y$")            -- Yank to end of line
-map("x", "y", "mzy`z")         -- return to original position after yanking
-map("n", "U", "<C-r>")         -- Redo
-map("x", "p", "P")             -- Prevent register changes when pasting Visual to allow continuous pasting
-map("n", "p", "]p`]")          -- Auto align indentation of paste and move to the end
+map("n", "Y", "y$") -- Yank to end of line
+map("x", "y", "mzy`z") -- return to original position after yanking
+map("n", "U", "<C-r>") -- Redo
+map("x", "p", "P") -- Prevent register changes when pasting Visual to allow continuous pasting
+map("n", "p", "]p`]") -- Auto align indentation of paste and move to the end
 map("n", "P", "]P`]")
-map("", "gV", "`[v`]")         -- Reselect the previous Visual selection
+map("", "gV", "`[v`]") -- Reselect the previous Visual selection
 map({ "o", "x" }, "i<Space>", "iW")
-map({ 'n', 'o', 'v' }, 'M', "expand('<cword>') =~# 'end' ? '%' : 'g%'", { expr = true, remap = true })
+map({ "n", "o", "v" }, "M", "expand('<cword>') =~# 'end' ? '%' : 'g%'", { expr = true, remap = true })
 -- map({ 'n', 'o', 'v' }, 'M', '%', { remap = true })
 map("n", "F<CR>", "{")
 map("n", "f<CR>", "}")
@@ -32,10 +32,10 @@ map("n", "<C-j>", function() -- Move current line down
 end, { expr = true, silent = true })
 map("x", "<C-k>", ":move'<-2<CR>gv=gv", { silent = true }) -- Move selected lines up
 map("x", "<C-j>", ":move'>+1<CR>gv=gv", { silent = true }) -- Move selected lines down
-map("n", "<Space>g", "<Cmd>copy.<CR>")                     -- Duplicate current line down
-map("n", "<Space>G", "<Cmd>copy-1<CR>")                    -- Duplicate current line up
-map("x", "<Space>g", ":copy'<-1<CR>gv")                    -- Duplicate selected lines up
-map("x", "<Space>G", ":copy'>+0<CR>gv")                    -- Duplicate selected lines down
+map("n", "<Space>g", "<Cmd>copy.<CR>") -- Duplicate current line down
+map("n", "<Space>G", "<Cmd>copy-1<CR>") -- Duplicate current line up
+map("x", "<Space>g", ":copy'<-1<CR>gv") -- Duplicate selected lines up
+map("x", "<Space>G", ":copy'>+0<CR>gv") -- Duplicate selected lines down
 
 -- Tab Management
 map("n", "<Tab>", "<Cmd>tabnext<CR>", opts)
@@ -49,7 +49,7 @@ map("n", "tq", "<Cmd>tabclose<CR>", opts)
 map("n", "tm", "<Cmd>tab term<CR>", opts)
 
 -- Replace / Substitute
-map("n", "S", [[:%s/\V\<<C-r><C-w>\>//g<Left><Left>]])                                    -- Substitute word under cursor in file
+map("n", "S", [[:%s/\V\<<C-r><C-w>\>//g<Left><Left>]]) -- Substitute word under cursor in file
 map("x", "S", [["zy:%s/\V<C-r><C-r>=escape(@z, '/\\')<CR>//gce<Left><Left><Left><Left>]]) -- Substitute selected text in file (confirm)
 
 -- Search
@@ -66,19 +66,20 @@ map("x", ">", ">gv", opts)
 
 -- Smart Insert / Case Change
 map("n", "i", function() -- Smart insert on empty line
-    if vim.fn.empty(vim.fn.getline('.')) == 1 then
+    if vim.fn.empty(vim.fn.getline(".")) == 1 then
         return '"_cc'
     end
-    return 'i'
+    return "i"
 end, { expr = true })
 map("n", "A", function() -- Smart append on empty line
-    if vim.fn.empty(vim.fn.getline('.')) == 1 then
+    if vim.fn.empty(vim.fn.getline(".")) == 1 then
         return '"_cc'
     end
-    return 'A'
+    return "A"
 end, { expr = true })
 map("i", "<C-g><C-u>", "<ESC>gUiwgi")
 map("i", "<C-g><C-k>", "<ESC>bgUlgi")
+
 -- Command abbreviations
 vim.cmd.cnoreabbrev("tn tabnew")
 vim.cmd.cnoreabbrev("vs vsplit")
