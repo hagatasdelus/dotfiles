b7b0074f54c5c389192edbc89954a15679ffa898
HaGaTa_ <hagatasdelus@gmail.com>
2025-12-13 23:49:45 +0900

    chore(nvim): update skk

--- common/config/nvim/lua/plugins/skkeleton.lua
+++ common/config/nvim/lua/plugins/skkeleton.lua
@@ -9,25 +9,46 @@ return {
         "https://github.com/delphinus/skkeleton_indicator.nvim",
     },
     cond = not is_on_vscode(),
-    init = function()
-        vim.keymap.set("n", "<C-j>", "a<Plug>(skkeleton-toggle)", { noremap = true, silent = true })
-        vim.keymap.set({ "i", "c", "t" }, "<C-j>", "<Plug>(skkeleton-toggle)", { noremap = true, silent = true })
-    end,
+    keys = {
+        {
+            "<C-j>",
+            "a<Plug>(skkeleton-toggle)",
+            mode = "n",
+            remap = true,
+            silent = true,
+        },
+        {
+            "<C-j>",
+            "<Plug>(skkeleton-toggle)",
+            mode = { "i", "c", "t" },
+            remap = true,
+            silent = true,
+        },
+    },
+    -- init = function()
+    --     vim.keymap.set("n", "<C-j>", "a<Plug>(skkeleton-toggle)", { noremap = true, silent = true })
+    --     vim.keymap.set({ "i", "c", "t" }, "<C-j>", "<Plug>(skkeleton-toggle)", { noremap = true, silent = true })
+    -- end,
     config = function()
+        local db_path = jisyo_dir .. "/skkeleton.db"
         vim.fn["skkeleton#config"]({
+            databasePath = db_path,
+            sources = { "deno_kv" },
             eggLikeNewline = true,
             globalDictionaries = {
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.L"),
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.emoji.utf8"),
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.law"),
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.propernoun"),
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.geo"),
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.station"),
-                vim.fn.expand(jisyo_dir .. "/SKK-JISYO.jinmei"),
+                jisyo_dir .. "/SKK-JISYO.L",
+                jisyo_dir .. "/SKK-JISYO.emoji.utf8",
+                jisyo_dir .. "/SKK-JISYO.law",
+                jisyo_dir .. "/SKK-JISYO.propernoun",
+                jisyo_dir .. "/SKK-JISYO.geo",
+                jisyo_dir .. "/SKK-JISYO.station",
+                jisyo_dir .. "/SKK-JISYO.jinmei",
             },
-            userDictionary = vim.fn.expand(jisyo_dir .. "/skk-jisyo.utf8"),
+            -- userDictionary = vim.fn.expand(jisyo_dir .. "/skk-jisyo.utf8"),
             -- completionRankFile = vim.fn.expand(jisyo_dir .. "/rank.json"),
             immediatelyCancel = false,
+            -- registerConvertResult = true,
         })
+        vim.fn["skkeleton#initialize"]()
     end,
 }
