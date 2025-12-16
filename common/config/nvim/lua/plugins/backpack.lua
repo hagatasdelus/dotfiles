c1f4994394391ac24fafac25d81b7cb1720184da
HaGaTa_ <hagatasdelus@gmail.com>
2025-12-16 20:18:39 +0900

    feat(nvim,backpack): add colorscheme backpack

---
+++ common/config/nvim/lua/plugins/backpack.lua
@@ -0,0 +1,49 @@
+return {
+    "https://github.com/Mitch1000/backpack.nvim",
+    event = { "UiEnter", "ColorScheme" },
+    cond = not is_on_vscode(),
+    priority = 1000,
+    build = ":BackpackCompile",
+    opts = function()
+        return {
+            theme = "dark",
+            compile = true,
+            transparent = true,
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none",
+                        },
+                    },
+                },
+            },
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    StatusLine = { link = "Normal" },
+                    StatusLineNC = { link = "Normal" },
+
+                    NormalFloat = { bg = "none" },
+                    FloatBorder = { bg = "none" },
+                    FloatTitle = { bg = "none" },
+
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+                    NoiceVirtualText = { bg = theme.ui.bg_search },
+                }
+            end,
+        }
+    end,
+    config = function(_, opts)
+        local backpack = require("backpack")
+        backpack.setup(opts)
+        vim.cmd.colorscheme("backpack")
+    end,
+}
