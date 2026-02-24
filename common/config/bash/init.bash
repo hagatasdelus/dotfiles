4029df775ff2f0d111873e5f49da861ed2cc129c
HaGaTa_ <hagatasdelus@gmail.com>
2026-02-24 23:56:47 +0900

    feat(bash): add CLI completions and pnpm path config

--- common/config/bash/init.bash
+++ common/config/bash/init.bash
@@ -30,6 +30,35 @@ function cd() {
     }
 }
 
+# pip
+if type pip &>/dev/null; then
+    function cache::pip() {
+        pip completion --bash > ${CACHE_PROFILE}/pip.bash
+    }
+    if [[ ! -f ${CACHE_PROFILE}/pip.bash ]]; then
+        cache::pip
+    fi
+    . ${CACHE_PROFILE}/pip.bash
+fi
+if type pip2 &>/dev/null; then
+    function cache::pip2() {
+        pip2 completion --bash > ${CACHE_PROFILE}/pip2.bash
+    }
+    if [[ ! -f ${CACHE_PROFILE}/pip2.bash ]]; then
+        cache::pip2
+    fi
+    . ${CACHE_PROFILE}/pip2.bash
+fi
+if type pip3 &>/dev/null; then
+    function cache::pip3() {
+        pip3 completion --bash > ${CACHE_PROFILE}/pip3.bash
+    }
+    if [[ ! -f ${CACHE_PROFILE}/pip3.bash ]]; then
+        cache::pip3
+    fi
+    . ${CACHE_PROFILE}/pip3.bash
+fi
+
 ### Mise ###
 if type mise &>/dev/null; then
     function cache::mise() {
@@ -39,6 +68,51 @@ if type mise &>/dev/null; then
         cache::mise
     fi
     . ${CACHE_PROFILE}/mise.bash
+fi
+
+# pnpm
+if [[ -d "${HOME}/.local/share/pnpm" ]]; then
+    export PNPM_HOME="${HOME}/.local/share/pnpm"
+    case ":$PATH:" in
+        *":$PNPM_HOME:"*) ;;
+        *) export PATH="$PNPM_HOME:$PATH" ;;
+    esac
+fi
+
+# Docker
+if [[ -d /Applications/Docker.app ]]; then
+    alias docker=/Applications/Docker.app/Contents/Resources/bin/docker
+fi
+if type docker &>/dev/null; then
+    function cache::docker() {
+        docker completion bash > ${CACHE_PROFILE}/docker.bash
+    }
+    if [[ ! -f ${CACHE_PROFILE}/docker.bash ]]; then
+        cache::docker
+    fi
+    . ${CACHE_PROFILE}/docker.bash
+fi
+
+# kubectl
+if type kubectl &>/dev/null; then
+    function cache::kubectl() {
+        kubectl completion bash > ${CACHE_PROFILE}/kubectl.bash
+    }
+    if [[ ! -f ${CACHE_PROFILE}/kubectl.bash ]]; then
+        cache::kubectl
+    fi
+    . ${CACHE_PROFILE}/kubectl.bash
+fi
+
+# Deno
+if type deno &>/dev/null; then
+    function cache::deno() {
+        deno completions bash > ${CACHE_PROFILE}/deno.bash
+    }
+    if [[ ! -f ${CACHE_PROFILE}/deno.bash ]]; then
+        cache::deno
+    fi
+    . ${CACHE_PROFILE}/deno.bash
 fi
 
 ### Starship ###
