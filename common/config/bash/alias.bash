ef1ee94957a79a7dac529b8253b1b070ce671366
HaGaTa_ <hagatasdelus@gmail.com>
2026-03-31 22:39:13 +0900

    feat: add fv and fg - imadake

--- common/config/bash/alias.bash
+++ common/config/bash/alias.bash
@@ -3,5 +3,7 @@ alias eza='eza -l --header --icons'
 alias b2a='sh ${HOME}/bin/b2a.sh'
 alias auth='sh ${HOME}/bin/auth.sh'
 alias karabiner_cli="/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
+alias fv='rg --files --hidden --glob "!.git" | fzf --preview "bat --style=numbers --color=always --line-range :500 {}" --bind "enter:become(nvim {})"'
+alias fg='fzf --bind "change:reload:rg --line-number --no-heading --color=always --smart-case {q} || true" --ansi --delimiter : --preview "bat --style=numbers --color=always --highlight-line {2} {1}" --preview-window "up,60%,border-bottom,+{2}+3/3,~3" --bind "enter:become(nvim +{2} {1})"'
 
 alias arto='/Applications/Arto.app/Contents/MacOS/arto'
