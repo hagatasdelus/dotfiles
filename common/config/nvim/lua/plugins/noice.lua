14d78688cfc0f852c353516a3f0b7b60bdd644c0
HaGaTa_ <hagatasdelus@gmail.com>
2026-05-08 23:03:53 +0900

    fix: typo

--- common/config/nvim/lua/plugins/noice.lua
+++ common/config/nvim/lua/plugins/noice.lua
@@ -8,7 +8,7 @@ return {
     opts = function()
         return {
             lsp = {
-                overrice = {
+                override = {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                     ["vim.lsp.util.stylize_markdown"] = true,
                     ["cmp.entry.get_documentation"] = true,
