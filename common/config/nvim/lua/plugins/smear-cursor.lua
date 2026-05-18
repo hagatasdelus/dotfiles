8a58be4fca123b5bc113738c43811384de7247e5
HaGaTa_ <hagatasdelus@gmail.com>
2026-05-18 17:11:40 +0900

    feat(nvim): smear-cursor unabled

--- common/config/nvim/lua/plugins/smear-cursor.lua
+++ common/config/nvim/lua/plugins/smear-cursor.lua
@@ -1,14 +1,15 @@
 return {
     "https://github.com/sphamba/smear-cursor.nvim",
+    enabled = false,
     event = "VeryLazy",
     opts = {
-        -- stiffness = 0.7,
-        -- trailing_stiffness = 0.6,
-        -- stiffness_insert_mode = 0.7,
-        -- trailing_stiffness_insert_mode = 0.7,
-        -- matrix_pixel_threshold = 0.5,
-        -- damping = 0.95,
-        -- damping_insert_mode = 0.95,
-        -- time_interval = 7,
+        stiffness = 0.7,
+        trailing_stiffness = 0.6,
+        stiffness_insert_mode = 0.7,
+        trailing_stiffness_insert_mode = 0.7,
+        matrix_pixel_threshold = 0.5,
+        damping = 0.95,
+        damping_insert_mode = 0.95,
+        time_interval = 7,
     },
 }
