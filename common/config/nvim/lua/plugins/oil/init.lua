f68d2a22bacb66781200b9c3f469cf35f9659bcc
HaGaTa_ <hagatasdelus@gmail.com>
2026-06-10 00:43:53 +0900

    style(nvim): escape C-t to C-b

--- common/config/nvim/lua/plugins/oil/init.lua
+++ common/config/nvim/lua/plugins/oil/init.lua
@@ -9,7 +9,7 @@ return {
     },
     keys = {
         {
-            "<leader>nn",
+            ",b",
             function()
                 vim.cmd.Oil()
             end,
@@ -72,7 +72,7 @@ return {
                 ["g."] = "actions.toggle_hidden",
                 ["<C-s>"] = "actions.select_vsplit",
                 ["<C-h>"] = "actions.select_split",
-                ["<C-t>"] = "actions.select_tab",
+                ["<C-b>"] = "actions.select_tab",
             },
             view_options = {
                 show_hidden = true,
